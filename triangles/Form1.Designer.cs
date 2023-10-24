a32bdbaea0461a803c04855d12502e2440c602df
Jeeppie <128357441+Jeeppie@users.noreply.github.com>
2023-10-24 15:15:21 +0300

    Main1.0
    MainVersion

---
+++ triangles/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace triangles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBoxValueA = new System.Windows.Forms.TextBox();
+            this.TextBoxValueC = new System.Windows.Forms.TextBox();
+            this.TextBoxValueB = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Проверка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(687, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "c";
+            // 
+            // TextBoxValueA
+            // 
+            this.TextBoxValueA.Location = new System.Drawing.Point(71, 64);
+            this.TextBoxValueA.Name = "TextBoxValueA";
+            this.TextBoxValueA.Size = new System.Drawing.Size(60, 20);
+            this.TextBoxValueA.TabIndex = 4;
+            this.TextBoxValueA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TextBoxValueA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxValueA_KeyPress);
+            // 
+            // TextBoxValueC
+            // 
+            this.TextBoxValueC.Location = new System.Drawing.Point(663, 64);
+            this.TextBoxValueC.Name = "TextBoxValueC";
+            this.TextBoxValueC.Size = new System.Drawing.Size(60, 20);
+            this.TextBoxValueC.TabIndex = 5;
+            this.TextBoxValueC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TextBoxValueB
+            // 
+            this.TextBoxValueB.Location = new System.Drawing.Point(389, 64);
+            this.TextBoxValueB.Name = "TextBoxValueB";
+            this.TextBoxValueB.Size = new System.Drawing.Size(60, 20);
+            this.TextBoxValueB.TabIndex = 6;
+            this.TextBoxValueB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(176, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.TextBoxValueB);
+            this.Controls.Add(this.TextBoxValueC);
+            this.Controls.Add(this.TextBoxValueA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextBoxValueA;
+        private System.Windows.Forms.TextBox TextBoxValueC;
+        private System.Windows.Forms.TextBox TextBoxValueB;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ triangles/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace triangles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TextBoxValueA.Text = "0";
+            TextBoxValueB.Text = "0";
+            TextBoxValueC.Text = "0";
+
+        }
+
+        double a, b, c;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TextBoxValueA.Text = "0";
+            TextBoxValueB.Text= "0";
+            TextBoxValueC.Text = "0";
+            label4.Text = "Результат:";
+        }
+
+        private void TextBoxValueA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9') || (e.KeyChar == '.') || e.KeyChar == (char)8)
+            {
+                
+                return;
+            }
+            e.Handled = true;
+            label4.Text = "Даун? Вводи числа/цифры";
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+           a = double.Parse(TextBoxValueA.Text);
+           b = double.Parse(TextBoxValueB.Text);
+           c = double.Parse(TextBoxValueC.Text);
+
+            if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a))
+            {
+                label4.Text = "Этот треугольник прямоугольный";
+            }
+            if ((a == b) || (a == c) || (b == c))
+            {
+                label4.Text = "Этот треугольник равнобедряный";
+            }
+            if ((a == b) && (a == c) && (b == c))
+            {
+                label4.Text = "Этот треугольник равносторонний";
+            }
+            if ((a + b < c) || (a + c < b) || (b + c < a) || (a <= 0) || (b <= 0) || (c <= 0))
+            {
+                label4.Text = "Такого треугольника не существует";
+            }
+           
+            else
+            {
+                label4.Text = "Это обычный треугольник";
+            }
+        }
+    }
+}
